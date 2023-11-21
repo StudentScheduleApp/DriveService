fea12be4570fb67139e05c54b109fd6303c5bee7
IlyaKrn <ilyakornoukhov@gmail.com>
2023-11-22 02:07:46 +0300

    log

--- src/main/java/com/studentscheduleapp/driveservice/api/ImageController.java
+++ src/main/java/com/studentscheduleapp/driveservice/api/ImageController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("api/")
@@ -22,16 +23,21 @@ public class ImageController {
 
     @PostMapping("upload")
     public ResponseEntity<String> upload(@RequestBody Byte[] file) {
-        if (file == null)
+        if (file == null || file.length == 0) {
+            Logger.getGlobal().info("bad request: file is null or empty");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
         String name = "";
         try {
             name = fileService.create(file);
         } catch (NullPointerException e){
+            Logger.getGlobal().info("bad request: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         } catch (Exception e) {
+            Logger.getGlobal().info("ERROR: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+        Logger.getGlobal().info("file " + name + " saved successful");
         return ResponseEntity.ok(name);
     }
 
@@ -41,6 +47,7 @@ public class ImageController {
         try {
             f = fileService.get(name);
         } catch (IOException e) {
+            Logger.getGlobal().info("ERROR: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
         if(f != null){
@@ -50,11 +57,14 @@ public class ImageController {
                 for (int i = 0; i < bs.length; i++){
                     Bs[i] = bs[i];
                 }
+                Logger.getGlobal().info("file " + name + " send successful");
                 return ResponseEntity.ok(Bs);
             } catch (IOException e) {
+                Logger.getGlobal().info("not found: file " + name + " not found");
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
             }
         }
+        Logger.getGlobal().info("not found: file " + name + " not found");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
@@ -63,8 +73,10 @@ public class ImageController {
         try {
             fileService.delete(name);
         } catch (IOException e) {
+            Logger.getGlobal().info("ERROR: " + e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+        Logger.getGlobal().info("file " + name + " deleted successful");
         return ResponseEntity.ok().build();
     }
 
